45749488d40c05ac59a5d7729da1fae8f6dd5df8
André Masson <amwebexpert@gmail.com>
2025-08-06 09:52:33 -0400

    feat: improve svg snapshot view

--- src/components/svg-snapshot.tsx
+++ src/components/svg-snapshot.tsx
@@ -20,6 +20,16 @@ const SvgSnapshot: FunctionComponent<SvgSnapshotProps> = ({
   const { width } = useWindowDimensions();
   const offScreenStyle = { position: 'absolute', left: width, top: 0 };
 
+  if (elements.length === 0) {
+    console.warn('[SvgSnapshot]: no elements provided');
+    return null;
+  }
+
+  if (!onBase64Generated) {
+    console.warn('[SvgSnapshot]: onBase64Generated callback not provided');
+    return null;
+  }
+
   // as soon as we have a ref to the generated svg, we can take a
   // snapshot and callback onBase64Generated with the result
   const onRefUpdate = (ref?: SvgRef | null) => {
@@ -27,7 +37,7 @@ const SvgSnapshot: FunctionComponent<SvgSnapshotProps> = ({
 
     if (Platform.OS === 'ios') {
       // Dont know why if we do not log this, the ref does not get updated
-      console.log('====>>> onRefUpdate', { elements: elements.length, scale, width: ref.props.width });
+      console.log('[SvgSnapshot] onRefUpdate', { elements: elements.length, scale, width: ref.props.width });
     }
     ref.toDataURL(onBase64Generated, DEFAULT_CANVAS_DIMENSIONS);
   };
